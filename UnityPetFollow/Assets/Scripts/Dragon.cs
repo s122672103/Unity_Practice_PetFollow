206cd8d7b0d7d21f856368cfe2968c1d0caf8c0a
OLIVER  HSU <s122672103@gmail.com>
2019-12-24 16:57:41 +0800

    寵物跟隨

---
+++ UnityPetFollow/Assets/Scripts/Dragon.cs
@@ -0,0 +1,30 @@
+﻿
+using UnityEngine;
+
+public class Dragon : MonoBehaviour
+{
+    public float speed= 1.5f;
+
+    public Transform Player;
+
+
+    private void LateUpdate()
+    {
+        Track();
+    }
+
+    public void Track() {
+
+        float h = Input.GetAxisRaw("Horizontal");
+
+        Vector3 posPlayer = Player.position;
+        Vector3 posDragon = transform.position;
+
+        transform.position = Vector3.Lerp(posDragon, posPlayer, 0.5f*Time.deltaTime*speed);
+
+        posDragon.y = posPlayer.y;
+        transform.LookAt(posPlayer);
+    }　
+
+
+}
